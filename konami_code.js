8eface5f5baa3089793e7576607468e7a07dc061
Michael McRay <mmcray5@gmail.com>
2018-07-10 00:34:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -16,7 +16,7 @@ function init() {
  document.body.addEventListener('keydown', function(e) {
   let i = 0
  
-    if (codes[i] === e.key, codes[i]) {
+    if (codes[i] === e.key) {
       i++;
       
     
